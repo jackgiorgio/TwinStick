126fb859461132d5fd6a98d507ba2f9a4caccb29
jackgiorgio <418682632@qq.com>
2018-04-19 18:41:57 +0800

    TwinSticker

---
+++ TwinSticks/Assets/Game/PlayerPrefsManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= 0f && volume <= 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("master volume out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnLockLevel(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(),1); //1 for true
+        }
+        else
+        {
+            Debug.LogError("trying of unblock level not in build order");
+        }
+
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+        bool isLevelUnblocked = (levelValue == 1);
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            return isLevelUnblocked;
+        }
+        else {
+            Debug.LogError("trying of unblock level not in build order");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty >= 0f && difficulty <= 3f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("difficulty out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+
+}
---
+++ TwinSticks/Assets/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording = true;
+    private float initialFixedDelta;
+    private bool isPause;
+
+    // Use this for initialization
+    void Start() {
+        PlayerPrefsManager.UnLockLevel(2);
+        initialFixedDelta = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            recording = false;
+        }
+        else
+        {
+            recording = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !isPause)
+        {
+            isPause = true;
+            PauseGame();     
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !isPause)
+        {
+            isPause = false;
+            ResumeGame();
+        }
+
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = initialFixedDelta;
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        isPause = pauseStatus;
+    }
+}
